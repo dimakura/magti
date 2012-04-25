2b964743b3399cf76fc9c82a530cb1391235925d
Dimitri Kurashvili <dimitri@c12.ge>
2012-04-25 17:47:57 +0400

    adding tracking method

--- lib/magti/sms.rb
+++ lib/magti/sms.rb
@@ -5,6 +5,17 @@ require 'httparty'
 require 'c12-commons'
 
 module Magti
+
+  private
+
+  def self.validate_security_options(options)
+    if options.nil? or options.empty? or Set[:username, :password, :service_id, :client_id] != Set.new(options.keys)
+      raise ArgumentError, 'specify Magti.config(options) with :username, :password, :client and :service keys'
+    end
+  end
+
+  public
+
   # Response object.
   # It contains information on response code and message id.
   class Response
@@ -21,14 +32,21 @@ module Magti
     raise ArgumentError, 'illegal mobile number' unless C12.correct_mobile?(mobile)
     raise ArgumentError, 'max size exceeded' if text.length > Magti::MAX_SIZE
     options = Magti.config.security_options
-    if options.nil? or options.empty? or Set[:username, :password, :service_id, :client_id] != Set.new(options.keys)
-      raise ArgumentError, 'specify Magti.config(options) with :username, :password, :client and :service keys'
-    end
+    Magti.validate_security_options(options)
     options = options.merge(:to => "995#{mobile}", :text => text)
-    resp = HTTParty.get(Magti::SMS_SEND_URL, options)
+    http_resp = HTTParty.get(Magti::SMS_SEND_URL, {:query => options}).body
     resp = Response.new
-    resp.code, resp.id = resp.split(' - ')
+    resp.code, resp.id = http_resp.split(' - ')
     resp
+  end
+
+  # Tracking SMS.
+  def self.track_sms(msg_id)
+    options = Magti.config.security_options
+    Magti.validate_security_options(options)
+    options = options.merge(:message_id => msg_id)
+    http_resp = HTTParty.get(Magti::SMS_TRACK_URL, {:query => options}).body
+    http_resp.to_i
   end
 
 end
--- lib/magti/version.rb
+++ lib/magti/version.rb
@@ -1,4 +1,4 @@
 # -*- encoding : utf-8 -*-
 module Magti
-  VERSION = "0.1.0.beta3"
+  VERSION = "0.1.0.beta5"
 end
